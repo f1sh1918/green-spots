026f83cb622b3590e709883cc867f0c58eb34a9d
Andy <fischer@integreat-app.de>
2025-11-24 15:13:14 +0100

    5: basic setup, add bottom navigation, add page widgets

---
+++ app/lib/spots/spots.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class Spots extends StatefulWidget {
+  const Spots({super.key});
+
+  @override
+  State<Spots> createState() => _SpotsState();
+}
+
+class _SpotsState extends State<Spots> {
+  @override
+  Widget build(BuildContext context) {
+    return Text('Spots Page');
+  }
+}
