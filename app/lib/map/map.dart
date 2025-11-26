44e96cf88955746d65ed9c26df27f12229b19ae6
Andy <fischer@integreat-app.de>
2025-11-26 18:38:24 +0100

    12: basic map integration

--- app/lib/map/map.dart
+++ app/lib/map/map.dart
@@ -1,4 +1,7 @@
+import 'dart:async';
+
 import 'package:flutter/material.dart';
+import 'package:maplibre_gl/maplibre_gl.dart';
 
 class MapPage extends StatefulWidget {
   const MapPage({super.key});
@@ -8,8 +11,19 @@ class MapPage extends StatefulWidget {
 }
 
 class _MapPageState extends State<MapPage> {
+
+  final _controllerCompleter = Completer<MapLibreMapController>();
+  static const _initial = CameraPosition(target: LatLng(48.3705, 10.8978), zoom: 10);
+  bool _styleLoaded = false;
   @override
   Widget build(BuildContext context) {
-    return Text('Map Page');
+    return MapLibreMap(
+      styleString: 'https://maps.tuerantuer.org/styles/integreat/style.json',
+      initialCameraPosition: _initial,
+      onMapCreated: (c) => _controllerCompleter.complete(c),
+      onStyleLoadedCallback: () => setState(() => _styleLoaded = true),
+    );
   }
 }
+
+
