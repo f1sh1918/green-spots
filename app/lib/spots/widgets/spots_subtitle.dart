741db4bbffa6226dbacda5ee1472a4ab46d3d7ef
Andy <fischer@integreat-app.de>
2026-04-16 10:20:06 +0200

    minor-fixes: adjust label

--- app/lib/spots/widgets/spots_subtitle.dart
+++ app/lib/spots/widgets/spots_subtitle.dart
@@ -63,7 +63,7 @@ class SpotsSubtitle extends StatelessWidget {
         color: Colors.blue,
         icon: Icons.water_drop,
         value: spot.water,
-        label: showLabel ? 'Wasser:' : null,
+        label: showLabel ? 'Wasserqualität:' : null,
         isNegative: spot.water == 'keine',
       ),
       if (spot.specials != null && spot.specials!.isNotEmpty) ...[
